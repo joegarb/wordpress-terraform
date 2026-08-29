a6fe0b1344bd803458eb74eafe9d629d4f093ced
Joe Garber <joegarb@users.noreply.github.com>
2026-08-29 11:09:31 +0100

    Add explicit types to all variables

--- modules/wordpress-all-in-one/variables.tf
+++ modules/wordpress-all-in-one/variables.tf
@@ -1,9 +1,11 @@
 variable "environment" {
   description = "Environment name which will be prefixed on all the resources to be created"
+  type        = string
 }
 
 variable "site_domain" {
   description = "The primary domain name of the website"
+  type        = string
 }
 
 variable "enable_public_ssh" {
@@ -26,75 +28,90 @@ variable "tags" {
 
 variable "instance_type" {
   description = "EC2 instance type"
+  type        = string
   default     = "t2.small"
 }
 
 variable "key_name" {
   description = "SSH key to attach to EC2 instance"
+  type        = string
   default     = null
 }
 
 variable "letsencrypt_email" {
   description = "Email address required to obtain a SSL cert from Lets Encrypt. If not specified, SSL will be disabled"
+  type        = string
   default     = null
 }
 
 variable "image" {
   description = "Docker image for WordPress"
+  type        = string
   default     = "wordpress"
 }
 
 variable "db_username" {
   description = "Database username for WordPress"
+  type        = string
   default     = "wordpress"
 }
 
 variable "db_password" {
   description = "Database password for WordPress"
+  type        = string
   default     = null
 }
 
 variable "wp_debug" {
   description = "Whether to enable WordPress debugging"
+  type        = number
   default     = 1
 }
 
 variable "wp_debug_log" {
   description = "Whether to write WordPress debug logs. Requires wp_debug to also be set."
+  type        = bool
   default     = true
 }
 
 variable "wp_extra" {
   description = "Extra config to go into wp-config.php"
+  type        = string
   default     = ""
 }
 
 variable "scheduled_stop_enabled" {
   description = "Whether to enable scheduled stop and start rules"
+  type        = bool
   default     = false
 }
 
 variable "scheduled_stop_timezone" {
   description = "Timezone for scheduled stop and start rules"
+  type        = string
   default     = "US/Eastern"
 }
 
 variable "scheduled_start" {
   description = "Scheduler expression for starting the instance (use along with cron_stop)"
+  type        = string
   default     = "cron(0 7 ? * MON-FRI *)"
 }
 
 variable "scheduled_stop" {
   description = "Scheduler expression for stopping the instance (such as during the night when it is not in use, or once weekly, etc)"
+  type        = string
   default     = "cron(0 3 ? * TUE-SAT *)"
 }
 
 variable "sftp_username" {
   description = "SFTP username"
+  type        = string
   default     = "ftpuser"
 }
 
 variable "sftp_password" {
   description = "SFTP password"
+  type        = string
   default     = null
 }
